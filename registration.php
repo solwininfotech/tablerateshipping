b04a925c98f15af5976823e89e7456791ae9246a
solwininfotech <info@solwininfotech.com>
2020-05-21 09:35:32 +0530

    initial commit

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin TableRateShipping Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_TableRateShipping
+ * @copyright  Copyright © 2006-2020 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_TableRateShipping',
+    __DIR__
+);
